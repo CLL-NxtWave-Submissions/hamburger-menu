21fb885e8a8d734225ae798f4f0994ceb66952c3
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 07:27:51 +0000

    Implemented the JSX element tree in Home component to render the intended UI

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,27 @@
-// Write your code here
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => {
+  const getWindowInnerWidth = () => window.innerWidth
+  const currentWindowInnerWidth = getWindowInnerWidth()
+
+  return (
+    <div className="home-container">
+      <Header />
+      <div className="home-content-container">
+        <img
+          className="home-content-img"
+          src={
+            currentWindowInnerWidth < 768
+              ? 'https://assets.ccbp.in/frontend/react-js/home-sm-img.png'
+              : 'https://assets.ccbp.in/frontend/react-js/home-lg-img.png'
+          }
+          alt="home"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Home
