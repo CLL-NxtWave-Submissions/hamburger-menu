b57c3d19afcfbf1fa4d4ea2578e431de86cd8590
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 07:33:24 +0000

    Implemented JSX element tree in About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,27 @@
-// Write your code here
+import Header from '../Header'
+
+import './index.css'
+
+const About = () => {
+  const getWindowInnerWidth = () => window.innerWidth
+  const currentWindowInnerWidth = getWindowInnerWidth()
+
+  return (
+    <div className="about-container">
+      <Header />
+      <div className="about-content-container">
+        <img
+          className="about-content-img"
+          src={
+            currentWindowInnerWidth < 768
+              ? 'https://assets.ccbp.in/frontend/react-js/about-sm-img.png'
+              : 'https://assets.ccbp.in/frontend/react-js/about-lg-img.png'
+          }
+          alt="about"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default About
