55aa2876c2d054baceaca13e8a589b33530b0941
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 07:42:50 +0000

    Implemented JSX element tree in NotFound component to render the intended UI

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,23 @@
-// Write your code here
+import Header from '../Header'
+
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <Header />
+    <div className="not-found-content-container">
+      <img
+        className="not-found-content-img"
+        src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+        alt="not found"
+      />
+      <h1 className="not-found-content-header">Lost Your Way?</h1>
+      <p className="not-found-content-description">
+        Sorry, we can't find that page. You'll find lots to explore on the home
+        page
+      </p>
+    </div>
+  </div>
+)
+
+export default NotFound
