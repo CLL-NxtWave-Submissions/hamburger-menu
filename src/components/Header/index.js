ac5780580ce487ce6371b225b65613363dc889f8
Surya T N <suryatej.nim@yahoo.com>
2022-11-02 13:33:19 +0000

    Added JSX button element around the hamburger menu icon and the corresponding CSS styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,58 +9,58 @@ import {BsInfoCircleFill} from 'react-icons/bs'
 import 'reactjs-popup/dist/index.css'
 import './index.css'
 
-const Header = () => {
-  const CustomGiHamburgerMenu = React.forwardRef(({open, ...props}, ref) => (
+const CustomGiHamburgerMenu = React.forwardRef(({open, ...props}, ref) => (
+  <button type="button" className="header-hamburger-menu-icon-btn">
     <GiHamburgerMenu
       className="header-hamburger-menu-icon"
       ref={ref}
       {...props}
     />
-  ))
+  </button>
+))
 
-  return (
-    <div className="header-container">
-      <button type="button" className="header-brand-button">
-        <img
-          className="header-brand-img"
-          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-          alt="website logo"
-        />
-      </button>
+const Header = () => (
+  <div className="header-container">
+    <button type="button" className="header-brand-button">
+      <img
+        className="header-brand-img"
+        src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+        alt="website logo"
+      />
+    </button>
 
-      <Popup
-        modal
-        trigger={open => <CustomGiHamburgerMenu open={open} />}
-        // className="popup-content"
-        closeOnDocumentClick
-      >
-        {close => (
-          <div className="popup-container">
-            <div className="popup-close-btn-container">
-              <button
-                className="popup-close-btn"
-                type="button"
-                onClick={() => close()}
-              >
-                <IoMdClose className="close-btn-icon" />
-              </button>
-            </div>
-            <div className="popup-menu-container">
-              <Link to="/" className="menu-item-container">
-                <AiFillHome className="menu-item-icon" />
-                <p className="menu-item-name">Home</p>
-              </Link>
-
-              <Link to="/about" className="menu-item-container">
-                <BsInfoCircleFill className="menu-item-icon" />
-                <p className="menu-item-name">About</p>
-              </Link>
-            </div>
+    <Popup
+      modal
+      trigger={open => <CustomGiHamburgerMenu open={open} />}
+      // className="popup-content"
+      closeOnDocumentClick
+    >
+      {close => (
+        <div className="popup-container">
+          <div className="popup-close-btn-container">
+            <button
+              className="popup-close-btn"
+              type="button"
+              onClick={() => close()}
+            >
+              <IoMdClose className="close-btn-icon" />
+            </button>
           </div>
-        )}
-      </Popup>
-    </div>
-  )
-}
+          <div className="popup-menu-container">
+            <Link to="/" className="menu-item-container">
+              <AiFillHome className="menu-item-icon" />
+              <p className="menu-item-name">Home</p>
+            </Link>
+
+            <Link to="/about" className="menu-item-container">
+              <BsInfoCircleFill className="menu-item-icon" />
+              <p className="menu-item-name">About</p>
+            </Link>
+          </div>
+        </div>
+      )}
+    </Popup>
+  </div>
+)
 
 export default Header
