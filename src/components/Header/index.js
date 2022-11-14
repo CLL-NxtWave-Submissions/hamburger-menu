22c1b976c27e8f54167d8d42cfda80b40d9f45ee
Surya T N <suryatej.nim@yahoo.com>
2022-11-14 13:12:29 +0000

    Changed Header component to be a class component to fix the forwardRef warning in the console, when using the GiHamburgerMenu component from react-icons package

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {Component} from 'react'
 import Popup from 'reactjs-popup'
 import {Link} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
@@ -9,68 +9,73 @@ import {BsInfoCircleFill} from 'react-icons/bs'
 import 'reactjs-popup/dist/index.css'
 import './index.css'
 
-const CustomGiHamburgerMenu = React.forwardRef(({open, ...props}, ref) => (
-  <button
-    testid="hamburgerIconButton"
-    type="button"
-    className="header-hamburger-menu-icon-btn"
-  >
-    <GiHamburgerMenu
-      className="header-hamburger-menu-icon"
-      ref={ref}
-      {...props}
-    />
-  </button>
-))
+// const CustomGiHamburgerMenu = React.forwardRef(({open, ...props}, ref) => (
+// const CustomGiHamburgerMenu = () => (
 
-const Header = () => (
-  <div className="header-container">
-    <Link to="/" className="routing-link">
-      <button type="button" className="header-brand-button">
-        <img
-          className="header-brand-img"
-          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-          alt="website logo"
-        />
-      </button>
-    </Link>
+// )
 
-    <Popup
-      modal
-      trigger={open => <CustomGiHamburgerMenu open={open} />}
-      className="popup-content"
-    >
-      {close => (
-        <div className="popup-container">
-          <div className="popup-close-btn-container">
+class Header extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="header-container">
+        <Link to="/" className="routing-link">
+          <button type="button" className="header-brand-button">
+            <img
+              className="header-brand-img"
+              src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+              alt="website logo"
+            />
+          </button>
+        </Link>
+
+        <Popup
+          modal
+          trigger={
             <button
-              testid="closeButton"
-              className="popup-close-btn"
+              // testid="hamburgerIconButton"
               type="button"
-              onClick={() => close()}
+              className="header-hamburger-menu-icon-btn"
             >
-              <IoMdClose className="close-btn-icon" />
+              <GiHamburgerMenu className="header-hamburger-menu-icon" />
             </button>
-          </div>
-          <ul className="popup-menu-container">
-            <li className="menu-item-container">
-              <AiFillHome className="menu-item-icon" />
-              <Link to="/" className="routing-link">
-                <p className="menu-item-name">Home</p>
-              </Link>
-            </li>
+          }
+          className="popup-content"
+        >
+          {close => (
+            <div className="popup-container">
+              <div className="popup-close-btn-container">
+                <button
+                  //   testid="closeButton"
+                  className="popup-close-btn"
+                  type="button"
+                  onClick={() => close()}
+                >
+                  <IoMdClose className="close-btn-icon" />
+                </button>
+              </div>
+              <ul className="popup-menu-container">
+                <li className="menu-item-container">
+                  <AiFillHome className="menu-item-icon" />
+                  <Link to="/" className="routing-link">
+                    <p className="menu-item-name">Home</p>
+                  </Link>
+                </li>
 
-            <li className="menu-item-container">
-              <BsInfoCircleFill className="menu-item-icon" />
-              <Link to="/about" className="routing-link">
-                <p className="menu-item-name">About</p>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
-    </Popup>
-  </div>
-)
+                <li className="menu-item-container">
+                  <BsInfoCircleFill className="menu-item-icon" />
+                  <Link to="/about" className="routing-link">
+                    <p className="menu-item-name">About</p>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </Popup>
+      </div>
+    )
+  }
+}
 
 export default Header
